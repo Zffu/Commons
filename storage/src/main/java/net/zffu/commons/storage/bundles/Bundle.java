ae4ee7b6e3966a5ae473f77e0083031459d17261
Zffu <radi0o0@outlook.com>
2024-10-10 01:02:27 +0200

    feat: started reworking bundles

--- storage/src/main/java/net/zffu/commons/storage/bundles/Bundle.java
+++ storage/src/main/java/net/zffu/commons/storage/bundles/Bundle.java
@@ -1,23 +1,39 @@
 package net.zffu.commons.storage.bundles;
 
 /**
- * <p>Base of a bundle. Allows to store an element of type K and another element of type T</p>
- * @param <K> the type of the first element
- * @param <T> the type of the second element
- * @since 1.0.0
+ * <p>Stores two elements of different data types.</p>
+ * @param <K> the first data type.
+ * @param <T> the second data type.
  */
-public interface Bundle<K, T> {
+public class Bundle<K, T> {
+
+    protected K k;
+    protected T t;
 
     /**
-     * <p>Gets the first element contained in the bundle.</p>
-     * @return the first element of type K
+     * <p>Creates a new bundle with the new provided elements.</p>
+     * @param k the first element.
+     * @param t the second element.
      */
-    K getFirst();
+    public Bundle(K k, T t) {
+        this.k = k;
+        this.t = t;
+    }
 
     /**
-     * <p>Gets the second element contained in the bundle.</p>
-     * @return the second element of type T
+     * <p>Gets the first element</p>
+     * @return the first element as K
      */
-    T getSecond();
+    public K getFirst() {
+        return this.k;
+    }
+
+    /**
+     * <p>Gets the second element.</p>
+     * @return the second element as T
+     */
+    public T getSecond() {
+        return this.t;
+    }
 
 }
