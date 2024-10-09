8ebdccdd968c1752fcd6a6c7fb0072311f414c83
Zffu <radi0o0@outlook.com>
2024-10-10 01:21:55 +0200

    feat: added data type

--- storage/src/main/java/net/zffu/commons/seal/Sealable.java
+++ storage/src/main/java/net/zffu/commons/seal/Sealable.java
@@ -1,15 +1,16 @@
 package net.zffu.commons.seal;
 
 /**
- * <p>An object that can be sealed to prevent further modifications</p>
+ * <p>An object that can be sealed to prevent further modifications.</p>
+ * @param <K> the target output class.
  */
-public interface Sealable {
+public interface Sealable<K extends Sealed> {
 
     /**
      * <p>Seals the object and internally prevents further modifications.</p>
      * @throws IllegalStateException if the object is already sealed.
      */
-    void seal() throws IllegalStateException;
+    K seal() throws IllegalStateException;
 
     /**
      * <p>Returns if the object is sealed or not</p>
