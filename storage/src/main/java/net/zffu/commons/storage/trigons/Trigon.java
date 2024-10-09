e2f0bf2f2529b9f91319d51b97ee8b4f4d857a56
Zffu <radi0o0@outlook.com>
2024-10-10 01:12:25 +0200

    feat: added trigon

---
+++ storage/src/main/java/net/zffu/commons/storage/trigons/Trigon.java
@@ -0,0 +1,51 @@
+package net.zffu.commons.storage.trigons;
+
+/**
+ * <p>Stores 3 elements of different data types.</p>
+ * @param <A> the first data type.
+ * @param <B> the second data type.
+ * @param <C> the third data type.
+ */
+public class Trigon<A, B, C> {
+
+    private A a;
+    private B b;
+    private C c;
+
+    /**
+     * <p>Creates a new {@link Trigon} with the provided elements.</p>
+     * @param a the first element.
+     * @param b the second element.
+     * @param c the third element.
+     */
+    public Trigon(A a, B b, C c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    /**
+     * <p>Gets the first element of the {@link Trigon}</p>
+     * @return the first element as A.
+     */
+    public A getFirst() {
+        return this.a;
+    }
+
+    /**
+     * <p>Gets the second element of the {@link Trigon}</p>
+     * @return the second element as B.
+     */
+    public B getSecond() {
+        return this.b;
+    }
+
+    /**
+     * <p>Gets the third element of the {@link Trigon}</p>
+     * @return the third element as C.
+     */
+    public C getThird() {
+        return this.c;
+    }
+
+}
